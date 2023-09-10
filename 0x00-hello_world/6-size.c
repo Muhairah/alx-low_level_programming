c05b12a6a5da5a6725dc96b682db83ed9bf637bc
Muhairah <m.alshareif@live.com>
2023-09-10 10:00:46 -0700

    size 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,13 +2,21 @@
 #include <limits.h>
 #include <float.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: A C program using sizeof print size of various tybes.
+ *
+ * Return: Always 0 (succesful)
+ */
+
 int main(void)
 {
-	printf("The size of char is %lu byte(s).\n", sizeof(char));
-	printf("The size of int is %lu byte(s).\n", sizeof(int));
-	printf("The size of long int is %lu byte(s).\n", sizeof(long int));
-	printf("The size of long long int is %lu byte(s).\n", sizeof(long long int));
-	printf("The size of float is %lu byte(s).\n", sizeof(float));
+	printf("The size of char is %u byte(s).\n", sizeof(char));
+	printf("The size of int is %u byte(s).\n", sizeof(int));
+	printf("The size of long int is %u byte(s).\n", sizeof(long int));
+	printf("The size of long long int is %u byte(s).\n", sizeof(long long int));
+	printf("The size of float is %u byte(s).\n", sizeof(float));
 
 return (0);
 }
