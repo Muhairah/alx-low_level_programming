992caeabafee37853ce91f9e307daf3422246627
Muhairah <m.alshareif@live.com>
2023-09-10 14:00:12 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -3,7 +3,7 @@
 /**
  * main - prints all possible different combinations of three digits
  * Return: Always 0 (Success)
-*/
+ */
 int main(void)
 {
 	int n, m, l;
@@ -31,4 +31,5 @@ int main(void)
 	putchar('\n');
 	return (0);
 }
+
 
