844edc7ffd3acc860b88e4b6f821217c24e65a5b
Muhairah <m.alshareif@live.com>
2023-10-11 06:15:35 -0700

    edit

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stdlib.h>
 #include <stddef.h>
 /**
  * init_dog - initialize a variable
@@ -13,6 +14,7 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 
 	if (d == NULL)
+		d = malloc(sizeof(struct dog));
 
 	d->name = name;
 	/**@*d:initializ of (name,age, owner)*/
--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "dog.h"
 /**
  * print_dog - function that prints a struct dog
@@ -7,14 +8,13 @@
  */
 void print_dog(struct dog *d)
 {
-
 	if (d == NULL)
 		return;
 
 	if (d->name == NULL)
-		d->name = "(nill)";
+		d->name = "(nil)";
 	if (d->owner == NULL)
-		d->owner = ("nill");
+		d->owner = "(nil)";
 
 	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
