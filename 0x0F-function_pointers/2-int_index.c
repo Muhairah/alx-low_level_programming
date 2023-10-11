c67ead97d2ffa0d8c9a2592354d1e71473c2927f
Muhairah <m.alshareif@live.com>
2023-10-11 07:09:31 -0700

    cmp

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,26 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - searches for int
+ * @array: array
+ * @size: size of array
+ * @cmp: pointer
+ *
+ * Return: 0 (always)
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+
+	int a;
+
+	if (array == NULL || size <= 0 || cmp == NULL)
+		return (-1);
+
+	for (a = 0; a < size; a++)
+	{
+
+		if (cmp(array[a]))
+			return (a);
+	}
+	return (-1);
+}
