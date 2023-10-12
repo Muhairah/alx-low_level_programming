96dfb0e9e9b3b9ea139629a0795519e0aa910e5e
Muhairah <m.alshareif@live.com>
2023-10-11 19:49:48 -0700

    edit calc

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,45 +1,42 @@
-#include "function_pointers.h"
 #include <stdlib.h>
 #include <stdio.h>
 #include "3-calc.h"
+
 /**
- * main - Prints the result of simple operations.
- * @argc: The number of arguments supplied to the program.
+ * main - Prints the result of simple operations
+ * @argc: The number of arguments supplied to the program
  * @argv: An array of pointers to the arguments.
  *
- * Return:0 ( Always)
+ * Return: Always 0.
  */
 int main(int argc, char *argv[])
 {
-	int num1, num2, answer;
-	int (*res)(int, int);
-	char *get_op;
+    int num1, num2;
+    char *op;
 
-	if (argc != 4)
-	{
-		printf("Error\n");
-		exit(98);
-	}
+    if (argc != 4)
+    {
+        printf("Error\n");
+        exit(98);
+    }
 
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[3]);
-	get_op = argv[2];
+    num1 = atoi(argv[1]);
+    op = argv[2];
+    num2 = atoi(argv[3]);
 
-	if (get_op_func(argv[2]) == NULL || argv[2][1] != '\0')
-	{
-		printf("Error\n");
-		exit(99);
-	}
+    if (get_op_func(op) == NULL || op[1] != '\0')
+    {
+        printf("Error\n");
+        exit(99);
+    }
 
-	if ((*get_op == '/' || *get_op == '%') && (*argv[3] == '0'))
-	{
-		printf("Error\n");
-		exit(100);
-	}
+    if ((*op == '/' && num2 == 0) || (*op == '%' && num2 == 0))
+    {
+        printf("Error\n");
+        exit(100);
+    }
 
-	res = get_op_func(get_op);
-	answer = res(num1, num2);
+    printf("%d\n", get_op_func(op)(num1, num2));
 
-	printf("%d\n", ans);
-	return (0);
+    return 0;
 }
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,4 +1,4 @@
-# include "3-calc.h"
+#include "3-calc.h"
 
 int op_add(int a, int b);
 int op_sub(int a, int b);
@@ -7,63 +7,58 @@ int op_div(int a, int b);
 int op_mod(int a, int b);
 
 /**
- * op_add - sum of numbers
- * @a: number one
- * @b: number tow
+ * op_add - Returns the sum of two numbers.
+ * @a: The first number.
+ * @b: The second number.
  *
- * Return: the sum of a and b
+ * Return: The sum of a and b.
  */
-int op_add(int a, int b);
+int op_add(int a, int b)
 {
-
 	return (a + b);
 }
 /**
- * op_sub - defferance of numbers
- * @a: number one
- * @b: number tow
+ * op_sub - Returns the difference of two numbers.
+ * @a: The first number.
+ * @b: The second number.
  *
- * Return: the diffrance of a and b
+ * Return: The difference of a and b.
  */
-int op_sub(int a, int b);
+int op_sub(int a, int b)
 {
-
 	return (a - b);
 }
 /**
- * op_mul - prouduct of numbers
- * @a: number one
- * @b: number tow
+ * op_mul - Returns the product of two numbers.
+ * @a: The first number.
+ * @b: The second number.
  *
- * Return: the prouduct of a and b
+ * Return: The product of a and b.
  */
-
-int op_mul(int a, int b);
+int op_mul(int a, int b)
 {
-
 	return (a * b);
 }
 /**
- * op_div - division of numbers
- * @a: number one
- * @b: number tow
+ * op_div - Returns the division of two numbers.
+ * @a: The first number.
+ * @b: The second number.
  *
- * Return: the division of a and b
+ * Return: The quotient of a and b.
  */
-int op_div(int a, int b);
+int op_div(int a, int b)
 {
-
 	return (a / b);
 }
 /**
- * op_mod - remainder of division of numbers
- * @a: number one
- * @b: number tow
+ * op_mod - Returns the remainder of the division of two numbers.
+ * @a: The first number.
+ * @b: The second number.
  *
- * Return: the remainder of division of a and b
+ * Return: The remainder of the division of a by b.
  */
-int op_mod(int a, int b);
+int op_mod(int a, int b)
 {
-
 	return (a % b);
 }
+
